ef6a36fe4819f7ce76f026f326965030647397f1
cbhower <39167861+cbhower@users.noreply.github.com>
2020-07-26 10:12:16 -0700

    model compiles with grayscale data

--- src/services/model.py
+++ src/services/model.py
@@ -17,22 +17,25 @@ def build_model(batch_size = 8, epochs = 100, split = .7):
     scores = []
 
     X, y = prepare_data.prepare()
-    X = X[:,:,:,:3]
+#     X = X[:,:,:,:3]
     training = prepare_data.prepare()
     train_pct_index = int(split * len(X))
+
     X_train, X_test = X[:train_pct_index], X[train_pct_index:]
     y_train, y_test = y[:train_pct_index], y[train_pct_index:]
-    print('succesfully prepared datasets')
-
+    
+    X_train, X_test = X_train.reshape((X_train.shape[0], 224, 224, 1)), X_test.reshape((X_test.shape[0], 224, 224, 1))
+#     y_train, y_test = y[:train_pct_index].reshape((X.shape[0], 224, 224, 1)), y[train_pct_index:].reshape((X.shape[0], 224, 224, 1))
+#     y_train, y_test = y[:train_pct_index].reshape((X.shape[0], 224, 224, 1)), y[train_pct_index:].reshape((X.shape[0], 224, 224, 1))
+#     X_train, X_test = X[:train_pct_index].reshape((X.shape[0], 224, 224, 1)), X[train_pct_index:].reshape((X.shape[0], 224, 224, 1))
 
     batch_size = batch_size
     num_classes = 11
     epochs = epochs
-    print(num_classes)
 
     ## define model hyperparams
     model = Sequential()
-    model.add(Conv2D(input_shape=(224,224,3),filters=64,kernel_size=(88,3),padding="same", activation="relu"))
+    model.add(Conv2D(input_shape=(224,224,1),filters=64,kernel_size=(88,3),padding="same", activation="relu"))
     model.add(Conv2D(filters=32,kernel_size=(224,3),padding="same", activation="relu"))
     model.add(MaxPool2D(pool_size=(2,2),strides=(2,2)))
     model.add(Conv2D(filters=64, kernel_size=(224,3), padding="same", activation="relu"))
--- src/services/prepare_data.py
+++ src/services/prepare_data.py
@@ -6,6 +6,10 @@ import numpy as np
 from runtime_constants import runtime_file, runtime_directories
 from services import fileservice
 from pathlib import Path
+from PIL import Image                   
+from skimage import color
+from skimage import io
+
 
 def prepare():
     """
@@ -25,9 +29,20 @@ def prepare():
                     runtime_file.CURRENT_EVALUATED_FILE_PATH = Path.joinpath(
                         runtime_directories.CURRENT_EVALUATED_ROOT_DIRECTORY, folder, file)
 
-                    img_array = imageio.imread(runtime_file.CURRENT_EVALUATED_FILE_PATH)
+                    # using imageio to read in data
+                    # img_array = imageio.imread(runtime_file.CURRENT_EVALUATED_FILE_PATH)
+                    # training_data.append([img_array,class_num])
+
+                    # using skimage
+                    img_array = color.rgb2gray(io.imread(runtime_file.CURRENT_EVALUATED_FILE_PATH))
                     training_data.append([img_array,class_num])
+
+                    # using pil to convert image to grayscale
+                    # img_array = Image.open(runtime_file.CURRENT_EVALUATED_FILE_PATH).convert('LA')
+                    # training_data.append([img_array,class_num])
+
                     # print(img_array)
+                    print('array shape', img_array.shape)
                     print(f'Sucessfully prepared {file}.')
                 except:
                     print(f'could not prepare file {file}')
@@ -54,9 +69,7 @@ def prepare():
         # print(y)
         y_df = pd.Series(y)
         y_df = pd.get_dummies(y_df)
-        print('dataframe', y_df)
         y = np.asarray(y_df)
-        print(y.shape)
 
         return X, y
 
