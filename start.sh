c91f1e3dff16ef1ebff4137ee87a2db8a8973c68
ANDERSON ANITO CRISTOVAO <57241826+underson14@users.noreply.github.com>
2020-11-03 14:30:16 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,19 +2,20 @@
 apt-get install python3-venv
 echo "Checking Python version..."
 
-if command -v python3 &>/dev/null; then
+if command -v python3.6 &>/dev/null; then
     echo "Found Python 3"
-    python3 -m venv ./venv
+    python3.6 -m venv ./venv
 
 
     if [ $? -eq 0 ]; then
         echo OK
         source venv/bin/activate
         pip install --upgrade pip
-        pip install numba==0.43.0 --user
-        pip install llvmlite==0.32.1 --user
+        pip install ipykernel
+        pip install numba==0.43.0
+        pip install llvmlite==0.32.1
         pip install -r /content/automatic-drum-transcription/requirements.txt
-        python3 /content/automatic-drum-transcription/src/start.py --folder=$1
+        python3.6 /content/automatic-drum-transcription/src/start.py --folder=$1
     else
         echo "Could not execute python -m venv ./venv"
         echo "Edit the script. Change the 'python' command to the one calling python3."
